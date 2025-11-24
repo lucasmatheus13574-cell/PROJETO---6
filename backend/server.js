2846ea46542fa197cee6d6eb96012188854e4562
Lucas Matheus <lucasmatheus135.74@gmail.com>
2025-11-23 23:22:50 -0300

    ultimo commit da noite

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false }
 });
 
 
