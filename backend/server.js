8c0d1143649ef8038a2e67b04e446fcec0d907c6
Lucas Matheus <lucasmatheus135.74@gmail.com>
2025-11-23 21:02:20 -0300

    servidor atualizado

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ dotenv.config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 
